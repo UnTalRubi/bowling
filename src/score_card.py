624e1600aa2f1fc13a650283921b85eed8f23617
Rubi Quintas Alonso <untalrubi@gmail.com>
2025-01-17 13:11:57 +0100

    feat(SCORE_CARD): :sparkles: Create get_pins and card functions.
    Created two functions in order to pass the initial tests. It checks the values of a new ScoreCard object.

---
+++ src/score_card.py
@@ -0,0 +1,10 @@
+class ScoreCard():
+
+    def __init__ (self, score_card):
+        self.pins = score_card
+
+    def card (self):
+        return ScoreCard
+    
+    def get_pins(self):
+        return self.pins
