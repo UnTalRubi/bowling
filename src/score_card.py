5edb4fb8351ab639a5b621c572bbf9fd77d7535e
UnTalRubi <untalrubi@gmail.com>
2025-01-21 13:35:18 +0100

    feat(SCORE_CARD): :sparkles: Finish decode spare.
    Finished refining the decode pins function to pass the spare test.

--- src/score_card.py
+++ src/score_card.py
@@ -1,22 +1,30 @@
 class ScoreCard():
     total_score = 0
-    pins_list = []
     ZERO = 0
     TEN = 10
 
     def __init__ (self, score_card):
         self.pins = score_card
+        self.pins_list = []
+        self.frames = []
 
     def __decode_pins__(self):
         for pin in self.pins:
             if pin.isdigit():
-                self.pins_list.append(int(pin)) 
+                pin = int(pin)
             elif pin == "-":
-                self.pins_list.append(self.ZERO)
+                pin = self.ZERO
             elif pin == "/":
-                self.pins_list.append(self.TEN - self.pins_list[-1])
-        return self.pins_list
+                pin = self.TEN - self.pins_list[-1]
+            self.pins_list.append(pin)
 
+        for pos in range(len(self.pins)):
+            if self.pins[pos] == "/":
+                self.pins_list[pos] += self.pins_list[pos + 1]
+
+        return self.pins_list
+    
+    
     def card(self):
         return ScoreCard
     
@@ -24,6 +32,5 @@ class ScoreCard():
         return self.pins
     
     def calculate_total(self):
-        self.__decode_pins__()
-        self.total_score = sum(self.pins_list)
-        return self.total_score
+        print(self.pins_list)
+        return sum(self.__decode_pins__())
