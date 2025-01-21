3524e07f5d65ddd394a9b45c4af3430ddf152961
KanekiiCat <alex123viri@gmail.com>
2025-01-21 16:41:02 +0100

    refactor(SCORE_CARD): :recycle: Refactor ScoreCard method.
    Refactored __decode_pins__ method and added a new comments.

--- src/score_card.py
+++ src/score_card.py
@@ -1,12 +1,21 @@
 class ScoreCard():
-    total_score = 0
     ZERO = 0
     TEN = 10
 
     def __init__ (self, score_card):
         self.pins = score_card
         self.pins_list = []
-        self.frames = []
+
+    def __decode_pins__(self):
+        for pin in self.pins:
+            self.pins_list.append(int(pin) if pin.isdigit() else self.ZERO if pin == "-" else self.TEN - self.pins_list[-1] if pin == "/" else self.TEN)
+        for pos in range(len(self.pins)):
+            self.pins_list[pos] += self.pins_list[pos + 1] if self.pins[pos] == "/" and pos + 2 < len(self.pins) else self.pins_list[pos + 1] + self.pins_list[pos + 2] if self.pins[pos] == "X" and pos + 3 < len(self.pins) else self.ZERO
+
+        return self.pins_list
+
+    '''
+    #Código extendido
 
     def __decode_pins__(self):
         for pin in self.pins:
@@ -23,17 +32,15 @@ class ScoreCard():
         for pos in range(len(self.pins)):
             if self.pins[pos] == "/" and pos + 2 < len(self.pins):
                 self.pins_list[pos] += self.pins_list[pos + 1]
+                
             if self.pins[pos] == "X" and pos + 3 < len(self.pins):
                 self.pins_list[pos] += self.pins_list[pos + 1] + self.pins_list[pos + 2]
 
-        return self.pins_list
-    
-    def card(self):
-        return ScoreCard
-    
+        return self.pins_list        
+    '''
+
     def get_pins(self):
         return self.pins
     
     def calculate_total(self):
-        print(self.pins_list)
         return sum(self.__decode_pins__())
