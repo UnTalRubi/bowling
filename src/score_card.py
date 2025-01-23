d0d6b3ed59fb699f43a023c910dd8edae3a8f35a
Rubi Quintas Alonso <untalrubi@gmail.com>
2025-01-23 13:01:11 +0100

    refactor(SCORE_CARD): :recycle: Encapsulate and rename
    Encapsulated aditional function from decode pins, renamed variables and alter the position of the constants.

--- src/score_card.py
+++ src/score_card.py
@@ -1,34 +1,40 @@
 class ScoreCard():
-    ZERO = 0
-    TEN = 10
 
     def __init__ (self, score_card):
         self.pins = score_card
         self.pins_list = []
 
-    def __decode_pins__(self):
+    def __decode_pins(self):
+        TEN = 10
+        ZERO = 0
         for pin in self.pins:
             if pin.isdigit():
                 pin = int(pin)
             elif pin == "-":
-                pin = self.ZERO
+                pin = ZERO
             elif pin == "/":
-                pin = self.TEN - self.pins_list[-1]
+                pin = TEN - self.pins_list[-1]
             elif pin == "X":
-                pin = self.TEN
+                pin = TEN
             self.pins_list.append(pin)
+        return self.pin_list
 
-        for pos in range(len(self.pins)):
-            if self.pins[pos] == "/" and pos + 2 < len(self.pins):
-                self.pins_list[pos] += self.pins_list[pos + 1]
+    def calculate_rolls(self):
+        for roll in range(self.get_number_rolls()):
+            if self.pins[roll] == "/" and roll + 2 < self.get_number_rolls():
+                self.pins_list[roll] += self.pins_list[roll + 1]
                 
-            if self.pins[pos] == "X" and pos + 3 < len(self.pins):
-                self.pins_list[pos] += self.pins_list[pos + 1] + self.pins_list[pos + 2]
+            if self.pins[roll] == "X" and roll + 3 < self.get_number_rolls():
+                self.pins_list[roll] += self.pins_list[roll + 1] + self.pins_list[roll + 2]
 
-        return self.pins_list        
+        return self.pins_list
 
     def get_pins(self):
         return self.pins
 
+    def get_number_rolls(self):
+        return len(self.pins)
+
     def calculate_total(self):
-        return sum(self.__decode_pins__())
+        self.__decode_pins()
+        return sum(self.__decode_pins())
